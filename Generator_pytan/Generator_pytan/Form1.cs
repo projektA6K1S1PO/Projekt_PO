5ce4a01cb95e1dff4fc391d92a96a6c6d4ba6350
Lukasz <Lukasz@LUKI_LAPTOK>
2018-11-11 19:18:08 +0100

    Zmiana tytulu aplikacji

--- Generator_pytan/Generator_pytan/Form1.cs
+++ Generator_pytan/Generator_pytan/Form1.cs
@@ -16,5 +16,10 @@ namespace Generator_pytan
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
