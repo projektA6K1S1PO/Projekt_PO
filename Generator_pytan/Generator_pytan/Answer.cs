b28d73751d06ebb5075b4ee32865b5155da9b53c
Piotr Korcz <piotr.korcz@student.wat.edu.pl>
2018-11-11 23:11:19 +0100

    dodanie parametrow do klas Answer i Question

---
+++ Generator_pytan/Generator_pytan/Answer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generator_pytan
+{
+    class Answer
+    { // atrybuty klasy
+        String content;// zmienna tresc odpowiedzi 
+        bool correctness;// zmienna boolowska prawda fałsza 
+        Answer(String content, bool correctness) {//konstruktor umozliwia stworzenie obiektu klasy Answer
+            
+            this.content = content;// przypisuje parametr content (okreslony  w konstruktorze ) do atrybutu klasy content 
+            this.correctness = correctness;// przypisuje parametr correctness (okreslony  w konstruktorze ) do atrybutu klasy correctness
+
+        }
+
+    }
+}
--- Generator_pytan/Generator_pytan/Question.cs
+++ Generator_pytan/Generator_pytan/Question.cs
@@ -7,6 +7,17 @@ using System.Threading.Tasks;
 namespace Generator_pytan
 {
     class Question
-    {
+    {   //atrybuty klasy
+        String content; //zmienna tresc pytania 
+        int points; //waga punktowa pytania 
+        List<Answer> answers; // lista obiektow odpoiwedz ( lista zawiera w sobie obiekty klasy Answer )
+        //Question(tu parametry konstruktora!)
+        public Question(String content, int points) {//publiczny czyli jest widoczny dla innych klas, konstruktor umozliwia stworzenie obiektu klasy Question
+            this.content = content; // przypisuje parametr content (okreslony  w konstruktorze ) do atrybutu klasy content 
+            this.points = points; // przypisuje parametr points (okreslony  w konstruktorze ) do atrybutu klasy points
+            answers = new List<Answer>();  //inicjalizacja listy 
+
+        }
+
     }
 }
