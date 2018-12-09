0c5b72b9879ca76a1692816a95776af1859c982d
Lukasz <Lukasz@LUKI_LAPTOK>
2018-12-09 11:59:32 +0100

    Praca nad form test

--- Generator_pytan/Generator_pytan/Test.Designer.cs
+++ Generator_pytan/Generator_pytan/Test.Designer.cs
@@ -29,39 +29,165 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.start_test = new System.Windows.Forms.Button();
+            this.stop_test = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numer_pytania = new System.Windows.Forms.Label();
+            this.odp_1 = new System.Windows.Forms.CheckBox();
+            this.odp_2 = new System.Windows.Forms.CheckBox();
+            this.odp_3 = new System.Windows.Forms.CheckBox();
+            this.odp_4 = new System.Windows.Forms.CheckBox();
+            this.odp_5 = new System.Windows.Forms.CheckBox();
+            this.nastepne_pyt = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(95, 231);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(190, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(52, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "label1";
             // 
-            // button1
+            // start_test
             // 
-            this.button1.Location = new System.Drawing.Point(65, 82);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(93, 68);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.start_test.Location = new System.Drawing.Point(193, 492);
+            this.start_test.Name = "start_test";
+            this.start_test.Size = new System.Drawing.Size(141, 87);
+            this.start_test.TabIndex = 2;
+            this.start_test.Text = "Rozpocznij rozwiązywanie testu";
+            this.start_test.UseVisualStyleBackColor = true;
+            this.start_test.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // stop_test
+            // 
+            this.stop_test.Location = new System.Drawing.Point(676, 492);
+            this.stop_test.Name = "stop_test";
+            this.stop_test.Size = new System.Drawing.Size(140, 87);
+            this.stop_test.TabIndex = 3;
+            this.stop_test.Text = "Zakończ test i zapisz wynik";
+            this.stop_test.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.odp_5);
+            this.panel1.Controls.Add(this.odp_4);
+            this.panel1.Controls.Add(this.odp_3);
+            this.panel1.Controls.Add(this.odp_2);
+            this.panel1.Controls.Add(this.odp_1);
+            this.panel1.Controls.Add(this.numer_pytania);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(193, 85);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(623, 401);
+            this.panel1.TabIndex = 4;
+            this.panel1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Pytanie";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rozwiązujesz zestaw: ";
+            // 
+            // numer_pytania
+            // 
+            this.numer_pytania.AutoSize = true;
+            this.numer_pytania.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numer_pytania.Location = new System.Drawing.Point(75, 21);
+            this.numer_pytania.Name = "numer_pytania";
+            this.numer_pytania.Size = new System.Drawing.Size(87, 29);
+            this.numer_pytania.TabIndex = 1;
+            this.numer_pytania.Text = "label4";
+            // 
+            // odp_1
+            // 
+            this.odp_1.AutoSize = true;
+            this.odp_1.Location = new System.Drawing.Point(41, 77);
+            this.odp_1.Name = "odp_1";
+            this.odp_1.Size = new System.Drawing.Size(18, 17);
+            this.odp_1.TabIndex = 2;
+            this.odp_1.UseVisualStyleBackColor = true;
+            // 
+            // odp_2
+            // 
+            this.odp_2.AutoSize = true;
+            this.odp_2.Location = new System.Drawing.Point(41, 143);
+            this.odp_2.Name = "odp_2";
+            this.odp_2.Size = new System.Drawing.Size(18, 17);
+            this.odp_2.TabIndex = 3;
+            this.odp_2.UseVisualStyleBackColor = true;
+            // 
+            // odp_3
+            // 
+            this.odp_3.AutoSize = true;
+            this.odp_3.Location = new System.Drawing.Point(41, 211);
+            this.odp_3.Name = "odp_3";
+            this.odp_3.Size = new System.Drawing.Size(18, 17);
+            this.odp_3.TabIndex = 4;
+            this.odp_3.UseVisualStyleBackColor = true;
+            // 
+            // odp_4
+            // 
+            this.odp_4.AutoSize = true;
+            this.odp_4.Location = new System.Drawing.Point(41, 282);
+            this.odp_4.Name = "odp_4";
+            this.odp_4.Size = new System.Drawing.Size(18, 17);
+            this.odp_4.TabIndex = 5;
+            this.odp_4.UseVisualStyleBackColor = true;
+            // 
+            // odp_5
+            // 
+            this.odp_5.AutoSize = true;
+            this.odp_5.Location = new System.Drawing.Point(43, 350);
+            this.odp_5.Name = "odp_5";
+            this.odp_5.Size = new System.Drawing.Size(18, 17);
+            this.odp_5.TabIndex = 6;
+            this.odp_5.UseVisualStyleBackColor = true;
+            // 
+            // nastepne_pyt
+            // 
+            this.nastepne_pyt.Location = new System.Drawing.Point(465, 492);
+            this.nastepne_pyt.Name = "nastepne_pyt";
+            this.nastepne_pyt.Size = new System.Drawing.Size(140, 87);
+            this.nastepne_pyt.TabIndex = 6;
+            this.nastepne_pyt.Text = "Następne pytanie";
+            this.nastepne_pyt.UseVisualStyleBackColor = true;
             // 
             // Test
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(640, 272);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(913, 622);
+            this.Controls.Add(this.nastepne_pyt);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.stop_test);
+            this.Controls.Add(this.start_test);
             this.Name = "Test";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Test_FormClosing);
             this.Load += new System.EventHandler(this.Test_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -69,6 +195,17 @@
 
         #endregion
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button start_test;
+        private System.Windows.Forms.Button stop_test;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox odp_4;
+        private System.Windows.Forms.CheckBox odp_3;
+        private System.Windows.Forms.CheckBox odp_2;
+        private System.Windows.Forms.CheckBox odp_1;
+        private System.Windows.Forms.Label numer_pytania;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox odp_5;
+        private System.Windows.Forms.Button nastepne_pyt;
     }
 }
--- Generator_pytan/Generator_pytan/Test.cs
+++ Generator_pytan/Generator_pytan/Test.cs
@@ -27,12 +27,17 @@ namespace Generator_pytan
 
         private void Test_Load(object sender, EventArgs e)
         {
-            label1.Text = baza_pytan_array[0,0];//Szybkie wyswietlenie sciezki do pliku
+            //label1.Text = baza_pytan_array[0,0];//Szybkie wyswietlenie sciezki do pliku
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
             
+        }
+
+        private void Test_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
